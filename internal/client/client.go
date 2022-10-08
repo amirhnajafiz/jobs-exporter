ca5f28764944756c50abcf805fba61f46d925763
amirhnajafiz <najafizadeh21@gmail.com>
2022-10-08 16:03:02 +0330

    add: client comments

--- internal/client/client.go
+++ internal/client/client.go
@@ -6,11 +6,16 @@ import (
 	"github.com/official-stallion/stallion"
 )
 
+// Client
+// manages the stallion golang skd connection
+// for blackbox status reporting.
 type Client struct {
 	Cfg        Config
 	Connection stallion.Client
 }
 
+// Connect
+// to stallion server.
 func (c *Client) Connect() error {
 	conn, err := stallion.NewClient(c.Cfg.Host + ":" + c.Cfg.Port)
 	if err != nil {
@@ -22,10 +27,14 @@ func (c *Client) Connect() error {
 	return nil
 }
 
+// Publish
+// send message over stallion.
 func (c *Client) Publish(topic string, payload []byte) error {
 	return c.Connection.Publish(topic, payload)
 }
 
+// Subscribe
+// subscribe over stallion.
 func (c *Client) Subscribe(topic string, channel chan []byte) {
 	c.Connection.Subscribe(topic, func(bytes []byte) {
 		channel <- bytes
