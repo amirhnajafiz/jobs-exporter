ac8e5a47fd9117d866e3329203cdb474a4145fce
amirhnajafiz <najafizadeh21@gmail.com>
2022-12-10 11:48:57 +0330

    add: provider

--- internal/cmd/main.go
+++ internal/cmd/main.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"encoding/json"
 	"time"
 
 	"github.com/official-stallion/stallion-black-box-exporter/internal/client"
@@ -11,6 +12,11 @@ import (
 const (
 	topic = "stallion-black-box"
 )
+
+type Payload struct {
+	Ok      bool      `json:"ok"`
+	Created time.Time `json:"created"`
+}
 
 // Execute method of blackbox.
 func Execute() {
@@ -54,7 +60,42 @@ func Execute() {
 	// activate client for providing.
 	{
 		go func() {
+			// creating a new client
+			cli := client.Client{
+				Cfg: cfg.Client,
+			}
 
+			// connecting client
+			for {
+				if er := cli.Connect(); er != nil {
+					blackBoxMetrics.ConsumeErrors.Add(1)
+
+					time.Sleep(2 * time.Second)
+
+					continue
+				}
+
+				break
+			}
+
+			for {
+				// generate a payload
+				p := Payload{
+					Ok:      true,
+					Created: time.Now(),
+				}
+
+				// convert to bytes
+				bytes, _ := json.Marshal(p)
+
+				// publish over stallion
+				if er := cli.Publish(topic, bytes); er != nil {
+					blackBoxMetrics.PublishErrors.Add(1)
+				}
+
+				// wait for 2 seconds
+				time.Sleep(2 * time.Second)
+			}
 		}()
 	}
 }
