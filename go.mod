d849ae871d9eb0c5b02d8076a4b93437d8b5990f
amirhnajafiz <najafizadeh21@gmail.com>
2022-12-10 11:28:56 +0330

    add: koanf load

--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module github.com/official-stallion/stallion-black-box-exporter
 
 go 1.19
 
-require github.com/official-stallion/stallion v1.2.2 // indirect
+require (
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/knadh/koanf v1.4.4 // indirect
+	github.com/mitchellh/copystructure v1.2.0 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/mitchellh/reflectwalk v1.0.2 // indirect
+	github.com/official-stallion/stallion v1.2.2 // indirect
+	golang.org/x/sys v0.0.0-20210603081109-ebe580a85c40 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,12 @@
 package config
 
 import (
+	"log"
+
+	"github.com/knadh/koanf"
+	"github.com/knadh/koanf/parsers/yaml"
+	"github.com/knadh/koanf/providers/file"
+	"github.com/knadh/koanf/providers/structs"
 	"github.com/official-stallion/stallion-black-box-exporter/internal/client"
 )
 
@@ -11,5 +17,23 @@ type Config struct {
 
 // Load configs.
 func Load() Config {
-	return Default()
+	var instance Config
+
+	k := koanf.New(".")
+
+	// load default configuration from file
+	if err := k.Load(structs.Provider(Default(), "koanf"), nil); err != nil {
+		log.Fatalf("error loading default: %s", err)
+	}
+
+	// load configuration from file
+	if err := k.Load(file.Provider("config.yaml"), yaml.Parser()); err != nil {
+		log.Printf("error loading config.yml: %s", err)
+	}
+
+	if err := k.Unmarshal("", &instance); err != nil {
+		log.Fatalf("error unmarshalling config: %s", err)
+	}
+
+	return instance
 }
