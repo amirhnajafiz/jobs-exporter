8bc473e31800f0e7e5ac8fcee38a8eab42d78854
amirhnajafiz <najafizadeh21@gmail.com>
2023-09-03 15:23:39 +0330

    add: k8s client connection

---
+++ internal/k8s/client.go
@@ -0,0 +1,20 @@
+package k8s
+
+import (
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func GetKubernetesClient(path string) (*kubernetes.Clientset, error) {
+	config, err := clientcmd.BuildConfigFromFlags("", path)
+	if err != nil {
+		return nil, err
+	}
+
+	cs, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	return cs, nil
+}
