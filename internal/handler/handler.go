53a5dbe21a434acf8ba58379f03cdf9286af480b
amirhnajafiz <najafizadeh21@gmail.com>
2023-09-14 17:51:38 +0330

    add: kafka handler integrated

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -2,14 +2,17 @@ package handler
 
 import (
 	"context"
+	"encoding/json"
 	"log"
 
+	"github.com/segmentio/kafka-go"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
 
 type Handler struct {
 	Namespace string
+	KafkaConn *kafka.Conn
 	K8SClient *kubernetes.Clientset
 }
 
@@ -17,11 +20,16 @@ func (h Handler) Monitor() {
 	ctx := context.Background()
 
 	for {
+		// get list of namespace jobs
 		jobs, err := h.K8SClient.BatchV1().Jobs(h.Namespace).List(ctx, v1.ListOptions{})
 		if err != nil {
 			log.Println(err)
 		}
 
+		// create messages array
+		var messages []kafka.Message
+
+		// iterate over existing jobs
 		for _, job := range jobs.Items {
 			p := &Pack{
 				Name:      job.GetName(),
@@ -29,6 +37,20 @@ func (h Handler) Monitor() {
 				Created:   job.GetCreationTimestamp().Time,
 				Status:    job.Status.String(),
 			}
+
+			bytes, er := json.Marshal(&p)
+			if er != nil {
+				log.Println(er)
+			}
+
+			messages = append(messages, kafka.Message{
+				Value: bytes,
+			})
+		}
+
+		// publish over kafka
+		if _, err = h.KafkaConn.WriteMessages(messages...); err != nil {
+			log.Println(err)
 		}
 	}
 }
