39b84d1ce27ffcc5d12128995375145141a13f45
amirhnajafiz <najafizadeh21@gmail.com>
2023-09-14 18:08:00 +0330

    add: main.go

--- main.go
+++ main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"github.com/amirhnajafiz/job-monitor/internal/config"
+	"github.com/amirhnajafiz/job-monitor/internal/handler"
+	"github.com/amirhnajafiz/job-monitor/internal/k8s"
+	"github.com/amirhnajafiz/job-monitor/internal/kafka"
+)
 
+func main() {
+	// load configs
+	cfg := config.Load()
+
+	// open kafka connection
+	k, err := kafka.GetConnection(cfg.Kafka)
+	if err != nil {
+		panic(err)
+	}
+
+	// create k8s client
+	c, err := k8s.GetClient(cfg.K8S)
+	if err != nil {
+		panic(err)
+	}
+
+	// create new handler
+	h := handler.Handler{
+		Interval:  cfg.Interval,
+		KafkaConn: k,
+		K8SClient: c,
+	}
+
+	// start monitor
+	h.Monitor()
 }
