62950c8a907d9f7c7ae181a7833f10e179844e2b
amirhnajafiz <najafizadeh21@gmail.com>
2023-09-14 18:05:39 +0330

    add: koanf config load

--- go.mod
+++ go.mod
@@ -3,8 +3,15 @@ module github.com/amirhnajafiz/job-monitor
 go 1.20
 
 require (
+	github.com/fatih/structs v1.1.0 // indirect
 	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/knadh/koanf v1.5.0 // indirect
+	github.com/mitchellh/copystructure v1.2.0 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/mitchellh/reflectwalk v1.0.2 // indirect
 	github.com/pierrec/lz4/v4 v4.1.15 // indirect
 	github.com/segmentio/kafka-go v0.4.42 // indirect
+	github.com/tidwall/pretty v1.2.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 	k8s.io/client-go v0.28.1 // indirect
 )
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,8 +1,22 @@
 package config
 
 import (
+	"encoding/json"
+	"log"
+	"strings"
+
 	"github.com/amirhnajafiz/job-monitor/internal/k8s"
 	"github.com/amirhnajafiz/job-monitor/internal/kafka"
+
+	"github.com/knadh/koanf"
+	"github.com/knadh/koanf/providers/env"
+	"github.com/knadh/koanf/providers/structs"
+	"github.com/tidwall/pretty"
+)
+
+const (
+	// Prefix indicates environment variables prefix.
+	Prefix = "JM_"
 )
 
 type Config struct {
@@ -11,6 +25,40 @@ type Config struct {
 	K8S      k8s.Config   `koanf:"cluster"`
 }
 
-func Load() {
+func Load() Config {
+	var instance Config
 
+	k := koanf.New(".")
+
+	// load default configuration from file
+	if err := k.Load(structs.Provider(Default(), "koanf"), nil); err != nil {
+		log.Fatalf("error loading default: %s", err)
+	}
+
+	// load environment variables
+	if err := k.Load(env.Provider(Prefix, ".", func(s string) string {
+		return strings.ReplaceAll(strings.ToLower(
+			strings.TrimPrefix(s, Prefix)), "__", ".")
+	}), nil); err != nil {
+		log.Printf("error loading environment variables: %s", err)
+	}
+
+	if err := k.Unmarshal("", &instance); err != nil {
+		log.Fatalf("error unmarshalling config: %s", err)
+	}
+
+	indent, err := json.MarshalIndent(instance, "", "\t")
+	if err != nil {
+		log.Fatalf("error marshaling config to json: %s", err)
+	}
+
+	indent = pretty.Color(indent, nil)
+	tmpl := `
+	================ Loaded Configuration ================
+	%s
+	======================================================
+	`
+	log.Printf(tmpl, string(indent))
+
+	return instance
 }
